3b6b52d8e2c5d8473b5dccac4b9ca0866aaa966d
Tia Hwang <tiahwang@Mac.attlocal.net>
2025-05-08 12:45:49 -0700

    Add: custom drawer

---
+++ lib/views/home/components/slider_drawer.dart
@@ -0,0 +1,79 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_todo/extensions/space_exs.dart';
+import 'package:flutter_todo/utils/app_colors.dart';
+
+class CustomDrawer extends StatelessWidget {
+  CustomDrawer({super.key});
+
+  // Icons
+  final List<IconData> icons = [
+    CupertinoIcons.home,
+    CupertinoIcons.person_fill,
+    CupertinoIcons.settings,
+    CupertinoIcons.info_circle_fill,
+  ];
+
+  // Texts
+  final List<String> texts = ["Home", "Profile", "Settings", "Details"];
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 90),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: AppColors.primaryGradientColor,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Column(
+        children: [
+          const CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(
+              "https://avatars.githubusercontent.com/u/90431155?v=4",
+            ),
+          ),
+          8.h,
+          Text("Tia Hwang", style: textTheme.displayMedium),
+          Text("Flutter dev", style: textTheme.displaySmall),
+
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+            width: double.infinity,
+            height: 300,
+            child: ListView.builder(
+              itemCount: icons.length,
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onTap: () {
+                    log('${texts[index]} Item Tapped!');
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(3),
+                    child: ListTile(
+                      leading: Icon(
+                        icons[index],
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      title: Text(
+                        texts[index],
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
