75789a0fb9d2e7974792f6cfb3c90b393f44ae9e
Tia Hwang <tiahwang@Mac.attlocal.net>
2025-05-09 08:53:32 -0700

    Add: task view page

--- lib/views/home/widget/fab.dart
+++ lib/views/home/widget/fab.dart
@@ -1,7 +1,7 @@
-import 'dart:developer';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/utils/app_colors.dart';
+import 'package:flutter_todo/views/tasks/widget/task_view.dart';
 
 class Fab extends StatelessWidget {
   const Fab({super.key});
@@ -11,7 +11,7 @@ class Fab extends StatelessWidget {
     return GestureDetector(
       onTap: () {
         // navigate to task view by tapping on this button later
-        log("Task view");
+        Navigator.push(context, CupertinoPageRoute(builder: (_) => TaskView()));
       },
       child: Material(
         borderRadius: BorderRadius.circular(15),
